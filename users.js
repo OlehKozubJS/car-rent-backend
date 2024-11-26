52d893079f8668f97f692893e25551d794a8ac9b
OlehKozubJS <oleg.kozub54@gmail.com>
2024-11-26 22:15:56 +0200

    Update users.js

--- users.js
+++ users.js
@@ -1,3 +1,10 @@
-const users = {};
+const admins = ["Alex", "Andrii", "Vasyl"];
+
+const clients = ["Anna", "Alina", "Tamara"];
+
+const users = {
+  admins,
+  clients,
+};
 
 module.exports = users;
