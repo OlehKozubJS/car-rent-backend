05bf45f5ddb09dcb48bd8d754803d1199acafcc4
OlehKozubJS <oleg.kozub54@gmail.com>
2024-11-26 23:14:17 +0200

    Update application.js

--- application.js
+++ application.js
@@ -1,9 +1,3 @@
 console.log("Welcome to Node.js, folks!");
 
-const users = require("./users");
-
-const { admins, clients } = users;
-
-console.log(admins);
-console.log(clients);
-console.table(users);
+const { writeFile, readFile } = require("fs");
