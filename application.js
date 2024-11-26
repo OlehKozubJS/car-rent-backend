e92b858772e5d16ef2cc2ec001351be4ba2a2406
OlehKozubJS <oleg.kozub54@gmail.com>
2024-11-26 23:06:47 +0200

    Create application.js

---
+++ application.js
@@ -0,0 +1,9 @@
+console.log("Welcome to Node.js, comrades");
+
+const users = require("./users");
+
+const { admins, clients } = users;
+
+console.log(admins);
+console.log(clients);
+console.table(users);
