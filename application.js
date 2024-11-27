ff8793c160bc344035572a65a6ecf3cc4ad25792
OlehKozubJS <oleg.kozub54@gmail.com>
2024-11-27 17:52:50 +0200

    Update application.js

--- application.js
+++ application.js
@@ -3,4 +3,13 @@ console.log("Welcome to Node.js, folks!");
 const { error } = require("console");
 const { writeFile, readFile } = require("fs/promises");
 
-const fileOperation = async () => {};
+const fileOperation = async () => {
+  try {
+    const data = await readFile("./users.json", "utf-8");
+    console.log(data);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+fileOperation();
