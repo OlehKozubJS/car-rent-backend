6ecfa2921524b0ea3f4d5af19e91328770af9e26
OlehKozubJS <oleg.kozub54@gmail.com>
2024-11-28 14:56:52 +0200

    Update application.js

--- application.js
+++ application.js
@@ -5,4 +5,14 @@ const cors = require("cors");
 
 const application = express();
 
+application.use(cors());
+
+application.get("/", (request, response) => {
+  response.send("<p>Hello dudes!</p>");
+});
+
+application.get("/get", (request, response) => {
+  response.send("<p>Hello! I am VioletFox! Progress Wins!</p>");
+});
+
 application.listen(3001);
