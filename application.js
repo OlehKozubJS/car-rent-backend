d438b08f4bce37973961a5b15a52153dac410cfd
OlehKozubJS <oleg.kozub54@gmail.com>
2024-11-28 15:34:57 +0200

    Update application.js

--- application.js
+++ application.js
@@ -37,7 +37,9 @@ application.get(
     const { params } = await request;
     const { first, second } = await params;
     await response.send(
-      `<p>${first}+${second}=${Number(first) + Number(second)}</p>`
+      `<p style="background-color: rgb(220, 220, 240); color: rgb(180, 160, 200);">${first}+${second}=${
+        Number(first) + Number(second)
+      }</p>`
     );
   })
 );
