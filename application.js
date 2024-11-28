fa7abd5febd9486b8e7fe8e45a4c19820d1a5789
OlehKozubJS <oleg.kozub54@gmail.com>
2024-11-28 15:15:18 +0200

    Update application.js

--- application.js
+++ application.js
@@ -3,6 +3,16 @@ const { writeFile, readFile, appendFile } = require("fs/promises");
 const express = require("express");
 const cors = require("cors");
 
+const errorHandler = (callback) => {
+  return async (request, response, next) => {
+    try {
+      callback(request, response);
+    } catch (error) {
+      next(error);
+    }
+  };
+};
+
 const application = express();
 
 application.use(cors());
@@ -13,6 +23,10 @@ application.get("/", (request, response) => {
 
 application.get("/get", (request, response) => {
   response.send("<p>Hello! I am VioletFox! Progress Wins!</p>");
+});
+
+application.post("", (request, response) => {
+  const {} = request;
 });
 
 application.listen(3001);
