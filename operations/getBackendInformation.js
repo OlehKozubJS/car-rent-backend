1f3fc82015a49ca7939262a2d4c9f88360317b36
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-01 00:05:33 +0200

    Update getBackendInformation.js

--- operations/getBackendInformation.js
+++ operations/getBackendInformation.js
@@ -1,11 +1,8 @@
 const { getCars } = require("./imports");
 
 const getBackendInformation = async (request, response) => {
-  await response.send(
-    `<p>Hello, dear comrades!</p><a href="/get">${JSON.stringify(
-      getCars()
-    )}</a>`
-  );
+  const cars = await String(getCars());
+  await response.send(`<p>Hello, dear comrades!</p><a href="/">${"cars"}</a>`);
 };
 
 module.exports = { getBackendInformation };
