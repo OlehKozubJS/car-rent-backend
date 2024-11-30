390788391ebae539f14fa811dc9c52f7fd06b2f1
OlehKozubJS <oleg.kozub54@gmail.com>
2024-11-30 20:58:09 +0200

    Update application.js

--- application.js
+++ application.js
@@ -4,42 +4,21 @@ const express = require("express");
 const cors = require("cors");
 
 const { errorHandler } = require("./hooks");
+const {
+  getMainBackendPage,
+  postBackendInformation,
+  getBackendInformation,
+  getServerState,
+} = require("./operations");
 
 const application = express();
 
 application.use(cors());
 
-application.get(
-  "/",
-  errorHandler(async (request, response) => {
-    await response.send(
-      '<p>Hello, dear comrades!</p><a href="/get">Get page</a>'
-    );
-  })
-);
+application.get("/", errorHandler(getMainBackendPage));
 
-application.get(
-  "/get",
-  errorHandler(async (request, response) => {
-    await response.send(
-      '<p>Hello! I am VioletFox! Progress Wins!</p><a href="/">Main page</a>'
-    );
-  })
-);
+application.get("/get", errorHandler(getBackendInformation));
 
-application.get(
-  "/post",
-  errorHandler(async (request, response) => {
-    const { query } = await request;
-    const { first, second } = await query;
-    await response.send(
-      `<p style="background-color: rgb(220, 220, 240); color: rgb(180, 160, 200);">${first}+${second}=${
-        Number(first) + Number(second)
-      }</p><a href="/">Main page</a>`
-    );
-  })
-);
+application.get("/post", errorHandler(postBackendInformation));
 
-application.listen(3001, () => {
-  console.log("Server is working!");
-});
+application.listen(3001, getServerState);
