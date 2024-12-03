6a499a684f2b769af2b5ad61df5122e6d74a5c34
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-04 00:02:48 +0200

    Update index.js

--- operations/index.js
+++ operations/index.js
@@ -1,11 +1,13 @@
 const { getMainBackendPage } = require("./getMainBackendPage");
 const { postBackendInformation } = require("./postBackendInformation");
 const { getBackendInformation } = require("./getBackendInformation");
+const { getCarsInformation } = require("./getCarsInformation");
 const { getServerState } = require("./getServerState");
 
 module.exports = {
   getMainBackendPage,
   postBackendInformation,
   getBackendInformation,
+  getCarsInformation,
   getServerState,
 };
