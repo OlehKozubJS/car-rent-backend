41930919df865b164575874af716c88390eb9705
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-04 01:50:32 +0200

    Update getMainBackendPage.js

--- operations/getMainBackendPage.js
+++ operations/getMainBackendPage.js
@@ -1,8 +1,11 @@
+const { getCarFilter } = require("./getCarFilter");
+
 const getMainBackendPage = async (request, response) => {
   await response.send(
     `<main>
       <p>Welcome! This is the backend page of the Car Rent project</p>
       <a href="/getCars">Information about every car</a>
+      ${getCarFilter}
     </main>`
   );
 };
