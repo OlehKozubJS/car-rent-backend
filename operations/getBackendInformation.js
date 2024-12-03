994e2bfa379f7013518f024ef8c193ab6e44f98e
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-03 22:31:08 +0200

    Update getBackendInformation.js

--- operations/getBackendInformation.js
+++ operations/getBackendInformation.js
@@ -1,13 +1,31 @@
 const { getCars } = require("./imports");
 
+const testCollection = [
+  {
+    firstObjectFirstPorperty: "firstObjectFirstValue",
+    firstObjectSecondProperty: "firstObjectSecondValue",
+    firstObjectThirdPorperty: "firstObjectThirdValue",
+  },
+  {
+    secondObjectFirstProperty: "secondObjectFirstValue",
+    secondObjectSecondProperty: "secondObjectSecondValue",
+    secondObjectThirdProperty: "secondObjectThirdValue",
+  },
+  {
+    thirdObjectFirstProperty: "",
+    thirdObjectThirdProperty: "thirdObjectThirdValue",
+  },
+];
+
 const getBackendInformation = async (request, response) => {
   const carsData = await getCars();
   const carsDataArray = JSON.parse(carsData);
   console.log(carsDataArray);
   await response.send(
     `<ul>${carsDataArray.map((carDataObject) => {
-      return `<li style="background-color: blue;>${Object.keys(carDataObject)
-      .map((carDataObjectKey) => {
+      return `<li style="background-color: blue;>${Object.keys(
+        carDataObject
+      ).map((carDataObjectKey) => {
         return `<p style="background-color: red;">${carDataObjectKey}: ${carDataObject[carDataObjectKey]}</p>`;
       })}</li>`;
     })}</ul>`
