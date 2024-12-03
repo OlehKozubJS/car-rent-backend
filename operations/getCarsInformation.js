4add67c92a6b9620f8bfa2cc8926abe4cc2b5189
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-03 23:58:07 +0200

    Update getCarsInformation.js

--- operations/getCarsInformation.js
+++ operations/getCarsInformation.js
@@ -1,6 +1,6 @@
 const { getCars } = require("./imports");
 
-const getBackendInformation = async (request, response) => {
+const getCarsInformation = async (request, response) => {
   const carsData = await getCars();
   const carsDataArray = JSON.parse(carsData);
   await response.send(
