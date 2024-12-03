9362aac0b2c75ef6c84c0ccb4851f180bfd18eb6
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-04 00:25:18 +0200

    Update getMainBackendPage.js

--- operations/getMainBackendPage.js
+++ operations/getMainBackendPage.js
@@ -1,6 +1,9 @@
 const getMainBackendPage = async (request, response) => {
   await response.send(
-    '<main><p>Hello, dear comrades!</p><a href="/getCars">Information about every car</a></main>'
+    `<main>
+      <p>Hello, dear comrades!</p>
+      <a href="/getCars">Information about every car</a>
+    </main>`
   );
 };
 
