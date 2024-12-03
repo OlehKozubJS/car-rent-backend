4f3d0e7f0ec209058da8e2a87dffdd8d6cba1252
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-04 01:15:26 +0200

    Update getCarInformation.js

--- operations/getCarInformation.js
+++ operations/getCarInformation.js
@@ -1,29 +1,13 @@
-const { getCars } = require("./imports");
-
-const getCarsInformation = async (request, response) => {
-  const carsInformationString = await getCars();
-  const carsInformationArray = JSON.parse(carsInformationString);
-  await response.send(
-    `<a href="/">Return to main page</a>
-
-    <h2>Cars information:</h2>
-
-    <ul>${carsInformationArray
-      .map((carInformationObject) => {
-        return `<li>
-          <ul>
-            ${Object.keys(carInformationObject)
-              .map((carInformationObjectKey) => {
-                return `<li>${carInformationObjectKey}:
-              ${carInformationObject[carInformationObjectKey]}</li>`;
-              })
-              .join("")}
-          </ul>  
-        </li>`;
-      })
-      .join("")}
-    </ul>`
-  );
+const getCarInformation = async (carInformationObject) => {
+  return `
+    <ul>
+      ${Object.keys(carInformationObject)
+        .map((carInformationObjectKey) => {
+          return `<li>${carInformationObjectKey}:
+        ${carInformationObject[carInformationObjectKey]}</li>`;
+        })
+        .join("")}
+    </ul>`;
 };
 
 module.exports = { getCarsInformation };
