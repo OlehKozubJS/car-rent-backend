ae572cb9dca7bbd34780c91feba4325eca0793a8
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-03 23:39:24 +0200

    Update getBackendInformation.js

--- operations/getBackendInformation.js
+++ operations/getBackendInformation.js
@@ -22,17 +22,19 @@ const getBackendInformation = async (request, response) => {
   const carsData = await getCars();
   const carsDataArray = JSON.parse(carsData);
   await response.send(
-    `<ul>${testCollection.map((carDataObject) => {
-      console.log(carDataObject);
-      return `<li style="background-color: rgb(230, 200, 200);"><ul>${Object.keys(
-        carDataObject
-      )
-        .map((carDataObjectKey) => {
-          console.log(carDataObjectKey);
-          return `<li style="background-color: rgb(200, 200, 230); border: 3px dashed rgb(200, 215, 200);">${carDataObjectKey}: ${carDataObject[carDataObjectKey]}</li>`;
-        })
-        .join("")}</ul></li>`;
-    })}</ul>`
+    `<ul>${testCollection
+      .map((carDataObject) => {
+        console.log(carDataObject);
+        return `<li style="background-color: rgb(230, 200, 200);"><ul>${Object.keys(
+          carDataObject
+        )
+          .map((carDataObjectKey) => {
+            console.log(carDataObjectKey);
+            return `<li style="background-color: rgb(200, 200, 230); border: 3px dashed rgb(200, 215, 200);">${carDataObjectKey}: ${carDataObject[carDataObjectKey]}</li>`;
+          })
+          .join("")}</ul></li>`;
+      })
+      .join("")}</ul>`
   );
 };
 
