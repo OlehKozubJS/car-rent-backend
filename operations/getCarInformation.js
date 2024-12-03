3bef7f3f4f4420606826c6dd65989dea5f777710
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-04 01:12:58 +0200

    Create getCarInformation.js

---
+++ operations/getCarInformation.js
@@ -0,0 +1,29 @@
+const { getCars } = require("./imports");
+
+const getCarsInformation = async (request, response) => {
+  const carsInformationString = await getCars();
+  const carsInformationArray = JSON.parse(carsInformationString);
+  await response.send(
+    `<a href="/">Return to main page</a>
+
+    <h2>Cars information:</h2>
+
+    <ul>${carsInformationArray
+      .map((carInformationObject) => {
+        return `<li>
+          <ul>
+            ${Object.keys(carInformationObject)
+              .map((carInformationObjectKey) => {
+                return `<li>${carInformationObjectKey}:
+              ${carInformationObject[carInformationObjectKey]}</li>`;
+              })
+              .join("")}
+          </ul>  
+        </li>`;
+      })
+      .join("")}
+    </ul>`
+  );
+};
+
+module.exports = { getCarsInformation };
