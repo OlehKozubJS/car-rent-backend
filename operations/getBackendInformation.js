2062c648bad3dd3de3425274ab3cd2480c3538a3
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-03 23:28:06 +0200

    Update getBackendInformation.js

--- operations/getBackendInformation.js
+++ operations/getBackendInformation.js
@@ -18,10 +18,25 @@ const testCollection = [
   },
 ];
 
+const testArray = [
+  "firstItem",
+  "secondItem",
+  "thirdItem",
+  "fourthItem",
+  "fifthItem",
+];
+
 const getBackendInformation = async (request, response) => {
   const carsData = await getCars();
   const carsDataArray = JSON.parse(carsData);
-  await response.send(
+  await response.send();
+};
+
+module.exports = { getBackendInformation };
+
+/*.filter((carDataObjectKey) => carDataObject[carDataObjectKey] !== ",")*/
+/*
+
     `<ul>${testCollection.map((carDataObject) => {
       console.log(carDataObject);
       return `<li style="background-color: rgb(230, 200, 200);"><ul>${Object.keys(
@@ -31,14 +46,5 @@ const getBackendInformation = async (request, response) => {
         return `<li style="background-color: rgb(200, 200, 230); border: 3px dashed rgb(200, 215, 200);">${carDataObjectKey}: ${carDataObject[carDataObjectKey]}</li>`;
       })}</ul></li>`;
     })}</ul>`
-  );
-};
-
-module.exports = { getBackendInformation };
-
-/*.filter((carDataObjectKey) => carDataObject[carDataObjectKey] !== ",")*/
-/*
-
-
 
 */
