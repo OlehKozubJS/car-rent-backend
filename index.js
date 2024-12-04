cc57c9432117b3e1648f059564f63713967dd74a
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-04 21:36:06 +0200

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-console.log("Welcome to Node.js, comrades");
-
-const users = require("./users");
-
-const { admins, clients } = users;
-
-console.log(admins);
-console.log(clients);
-console.table(users);
