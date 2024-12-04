4da346e2962613e3da271a5ad3f3809f0521b9c8
OlehKozubJS <oleg.kozub54@gmail.com>
2024-12-04 14:39:12 +0200

    Update getCarFilter.js

--- operations/getCarFilter.js
+++ operations/getCarFilter.js
@@ -3,7 +3,7 @@ const getCarFilter = () => {
     <div>
       <h3>Car brand (make)</h3>
       <label>
-        <input placeholder="Enter car make name or its part" />
+        <input placeholder="Enter car make name or its part" onChange="" />
       <label />
     </div>
     <div>
